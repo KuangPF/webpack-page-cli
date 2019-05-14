771b2e2ee433b6e553078ce451e5635c03704ef4
KuangPF <1633397286@qq.com>
2019-05-14 23:05:51 +0800

    mod: remove CommonsChunkPlugin

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -103,20 +103,20 @@ const webpackConfig = merge(baseWebpackConfig, {
     // enable scope hoisting
     new webpack.optimize.ModuleConcatenationPlugin(),
     // split vendor js into its own file
-    new webpack.optimize.CommonsChunkPlugin({
+    /* new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor',
       minChunks(module) {
         // any required modules inside node_modules are extracted to vendor
         return module.resource && /\.js$/.test(module.resource) && module.resource.indexOf(path.join(__dirname, '../node_modules')) === 0
       }
-    }),
+    }), */
     // extract webpack runtime and module manifest to its own file in order to
     // prevent vendor hash from being updated whenever app bundle is updated
-    new webpack.optimize.CommonsChunkPlugin({
+    /* new webpack.optimize.CommonsChunkPlugin({
       name: 'manifest',
       chunks: ['vendor'],
       minChunks: Infinity
-    }),
+    }), */
 
     // copy custom static assets
     new CopyWebpackPlugin([
