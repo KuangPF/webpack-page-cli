79dae49188ade508c6d866cd83b4fb4294244856
KuangPF <1633397286@qq.com>
2019-05-15 21:56:23 +0800

    feat: add util (clearConsole.js)

--- bin/index.js
+++ bin/index.js
@@ -5,13 +5,19 @@ program.version(require('../package').version).usage('<command> [options]')
 
 program
   .command('create <app-name>')
-  .description('create a new project powered by vue-cli-service')
+  .description('create a new project powered by webpack-page-cli')
+  .option('-f, --force [message]', 'Overwrite target directory if it exists')
   .action((name, cmd) => {
     const options = cleanArgs(cmd)
     require('../lib/create')(name, options)
   })
 
 program.parse(process.argv)
+
+// 将包名转化为驼峰命令方式  package-name => packageName
+function camelize (str) {
+  return str.replace(/-(\w)/g, (_, c) => c ? c.toUpperCase() : '')
+}
 
 function cleanArgs(cmd) {
   const args = {}
