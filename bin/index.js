f9ac12c199e652698a59188c7759166c60557d55
KuangPF <1633397286@qq.com>
2019-05-25 00:06:10 +0800

    chore: eslint config

--- bin/index.js
+++ bin/index.js
@@ -4,34 +4,10 @@ const chalk = require('chalk')
 const semver = require('semver')
 const requiredVersion = require('../package.json').engines.node
 
-function checkNodeVersion (wanted, id) {
-  if (!semver.satisfies(process.version, wanted)) {
-    console.log(chalk.red(
-      'You are using Node ' + process.version + ', but this version of ' + id +
-      ' requires Node ' + wanted + '.\nPlease upgrade your Node version.'
-    ))
-    process.exit(1)
-  }
-}
-checkNodeVersion(requiredVersion, 'vue-cli')
-program.version(require('../package').version).usage('<command> [options]')
-
-program
-  .command('create <app-name>')
-  .description('create a new project powered by webpack-page-cli')
-  .option('-f, --force [message]', 'Overwrite target directory if it exists')
-  .action((name, cmd) => {
-    const options = cleanArgs(cmd)
-    require('../lib/create')(name, options)
-  })
-
-program.parse(process.argv)
-
 // 将包名转化为驼峰命令方式  package-name => packageName
-function camelize (str) {
-  return str.replace(/-(\w)/g, (_, c) => c ? c.toUpperCase() : '')
+function camelize(str) {
+  return str.replace(/-(\w)/g, (_, c) => (c ? c.toUpperCase() : ''))
 }
-
 function cleanArgs(cmd) {
   const args = {}
   cmd.options.forEach(o => {
@@ -44,3 +20,23 @@ function cleanArgs(cmd) {
   })
   return args
 }
+function checkNodeVersion(wanted, id) {
+  if (!semver.satisfies(process.version, wanted)) {
+    console.log(chalk.red(`You are using Node ${process.version}, but this version of ${id} requires Node ${wanted} .\nPlease upgrade your Node version.`))
+    process.exit(1)
+  }
+}
+checkNodeVersion(requiredVersion, 'vue-cli')
+program.version(require('../package').version).usage('<command> [options]')
+
+program
+  .command('create <app-name>')
+  .description('create a new project powered by webpack-page-cli')
+  .option('-f, --force [message]', 'Overwrite target directory if it exists')
+  .action((name, cmd) => {
+    const options = cleanArgs(cmd)
+    /* eslint-disable global-require */
+    require('../lib/create')(name, options)
+  })
+
+program.parse(process.argv)
